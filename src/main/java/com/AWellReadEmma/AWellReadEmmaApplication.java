2e8644c7f71c5a25af5f3e7ab757c8bd8bc9acfc
egraham96 <eeg4@uw.edu>
2023-12-21 06:19:39 -0800

    Added starter files

---
+++ src/main/java/com/AWellReadEmma/AWellReadEmmaApplication.java
@@ -0,0 +1,13 @@
+package com.AWellReadEmma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AWellReadEmmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AWellReadEmmaApplication.class, args);
+	}
+
+}
